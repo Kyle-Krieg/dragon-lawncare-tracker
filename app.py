1097171f997efc81ba69ff6eb0adf25896378c1d
Kyle Krieg <kyle.krieg@live.com>
2026-02-26 22:46:21 -0500

    Added /tasks  endpoint and connected Flask to Postgres

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+
+import psycopg
+from psycopg.rows import dict_row
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 
-@app.route('/')
+def get_conn():
+    db_url = os.getenv("DATABASE_URL")
+    if not db_url:
+        raise RuntimeError("DATABASE_URL environment variable not set")
+    return psycopg.connect(db_url, row_factory=dict_row)
+
+@app.get("/")
 def home():
     return {"status": "lawncare backend is running"}
+
+@app.get("/tasks")
+def get_tasks():
+    with get_conn() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM task_list ORDER BY task_id")
+            tasks = cur.fetchall()
+    return jsonify(tasks)
